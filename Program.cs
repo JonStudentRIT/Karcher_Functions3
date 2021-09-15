dc35c94a1f973efe93025b5120248a49a51b727f
Jonathan Karcher <Jk9927@g.rit.edu>
2021-09-15 01:05:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Functions3
+{
+    /* Class: Program
+     * Author: Jonathan Karcher
+     * Purpose: Impersonate the Console.ReadLine() function
+     * Restrictions: None
+     */
+    class Program
+    {
+        // create a delegate function
+        delegate string impersonate();
+        static void Main(string[] args)
+        {
+            // create a string variable
+            string s = "";
+            // assign the delegated funciton to the imposter method
+            impersonate imposter = new impersonate(Imposter);
+            // use the imposter to store information from the user
+            s = imposter();
+            // output the information the user entered
+            Console.WriteLine(s);
+        }
+        /* Method: Imposter
+         * Purpose: Return anything the user entered when called
+         * Restrictions: None
+         */
+        static public string Imposter()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
